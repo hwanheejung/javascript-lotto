c4edec4530d01258b8622155ce9ba8e8b576651e
hwanheejung <hwanheejung117@gmail.com>
2025-02-22 20:59:09 +0900

    refactor: input, output 객체로 묶기

--- src/view/input.js
+++ src/view/input.js
@@ -1,21 +1,45 @@
-import validatePrice from "../validations/validatePrice.js";
-import validateRestart from "../validations/validateRestart.js";
-import {
-  validateBonusNumber,
-  validateWinningNumbers,
-} from "../validations/validateWinningNumbers.js";
+import useValidation from "./useValidation.js";
 import retryUntilValid from "./retryUntilValid.js";
 
-export const getLottoPrice = () =>
-  retryUntilValid("구입금액을 입력해 주세요. ", validatePrice);
+const input = {
+  /**
+   * @returns {number} - 구입 금액
+   */
+  getLottoPrice() {
+    const { validatePrice } = useValidation();
+    return retryUntilValid("구입 금액을 입력해 주세요: ", validatePrice);
+  },
 
-export const getWinningNumbers = () =>
-  retryUntilValid("\n당첨 번호를 입력해 주세요. ", validateWinningNumbers);
+  /**
+   * @returns {number[]} - 당첨 번호
+   */
+  getWinningNumbers() {
+    const { validateWinningNumbers } = useValidation();
+    return retryUntilValid(
+      "\n당첨 번호를 입력해 주세요: ",
+      validateWinningNumbers,
+    );
+  },
 
-export const getBonusNumber = (winningNumbers) =>
-  retryUntilValid("\n보너스 번호를 입력해 주세요. ", (bonusNumber) =>
-    validateBonusNumber(bonusNumber, winningNumbers),
-  );
+  /**
+   *
+   * @param {number[]} winningNumbers - 당첨 번호
+   * @returns {number} - 보너스 번호
+   */
+  getBonusNumber(winningNumbers) {
+    const { validateBonusNumber } = useValidation();
+    return retryUntilValid("\n보너스 번호를 입력해 주세요: ", (bonusNumber) =>
+      validateBonusNumber(bonusNumber, winningNumbers),
+    );
+  },
 
-export const getRestart = () =>
-  retryUntilValid("\n다시 시작하시겠습니까? (y/n) ", validateRestart);
+  /**
+   * @returns {boolean} - 재시작 여부
+   */
+  getRestart() {
+    const { validateRestart } = useValidation();
+    return retryUntilValid("\n다시 시작하시겠습니까? (y/n): ", validateRestart);
+  },
+};
+
+export default input;
