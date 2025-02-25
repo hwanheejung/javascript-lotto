376766d070e30ebb36eb54efe96174fa870710b2
hwanheejung <hwanheejung117@gmail.com>
2025-02-25 19:30:09 +0900

    feat: 당첨 번호 state에 저장

--- src/ui/components/WinningNumbersForm.js
+++ src/ui/components/WinningNumbersForm.js
@@ -2,6 +2,35 @@ import { Lotto, LottoNumber } from "../../constants/rules.js";
 import Component from "./Component.js";
 
 class WinningNumbersForm extends Component {
+  setup() {
+    this.state = {
+      winningNumbers: Array(Lotto.SIZE).fill(""),
+      bonusNumber: "",
+    };
+    this.events = {
+      "click@.open-result-button": this.openResult,
+      "input@.winning-numbers__input": this.handleInput,
+    };
+  }
+
+  openResult() {
+    console.log("당첨 번호:", this.state.winningNumbers);
+    console.log("보너스 번호:", this.state.bonusNumber);
+  }
+
+  handleInput(event) {
+    const { value, dataset } = event.target;
+    const isBonus = dataset.index;
+
+    if (!isBonus) {
+      const updatedWinningNumbers = [...this.state.winningNumbers];
+      updatedWinningNumbers[Number(dataset.index)] = value;
+      this.setState({ winningNumbers: updatedWinningNumbers });
+    } else {
+      this.setState({ bonusNumber: value });
+    }
+  }
+
   template() {
     return `
         <p class="winning-numbers__title">지난 주 당첨번호 6개와 보너스 번호 1개를 입력해주세요.</p>
@@ -9,13 +38,15 @@ class WinningNumbersForm extends Component {
             <div class="winning-numbers winning-numbers__main">
                 <span>당첨 번호</span>  
                 <div>
-                ${Array.from({ length: Lotto.SIZE }, () => {
+                ${Array.from({ length: Lotto.SIZE }, (_, index) => {
                   return `
                     <input 
                         type="number" 
                         class="winning-numbers__input" 
                         min="${LottoNumber.MIN}" 
                         max="${LottoNumber.MAX}" 
+                        value="${this.state.winningNumbers[index] || ""}"
+                        data-index="${index}"
                     />
                     `;
                 }).join("")}
@@ -30,11 +61,12 @@ class WinningNumbersForm extends Component {
                         class="winning-numbers__input" 
                         min="${LottoNumber.MIN}" 
                         max="${LottoNumber.MAX}" 
+                        value="${this.state.bonusNumber || ""}"
                     />
                 </div>
             </div>
         </div>
-        <button type="submit" class="open-result-button">결과 확인하기</button>
+        <button class="open-result-button">결과 확인하기</button>
     `;
   }
 }
